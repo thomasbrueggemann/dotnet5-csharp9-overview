98b214cdf11c1c6c2e326adf5517c64ce044727f
Thomas Brüggemann <mail@thomasbrueggemann.com>
2020-11-27 21:13:26 +0100

    Inits examples solution

---
+++ examples/b-Records/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+  _____                        _     
+ |  __ \                      | |    
+ | |__) |___  ___ ___  _ __ __| |___ 
+ |  _  // _ \/ __/ _ \| '__/ _` / __|
+ | | \ \  __/ (_| (_) | | | (_| \__ \
+ |_|  \_\___|\___\___/|_|  \__,_|___/
+
+*/
+
+#region What
+
+/*
+ * Top level programs allow us to easily start writing code
+ */
+
+#endregion
+
+#region Why
+
+#endregion
+
+using System;
+
+Console.WriteLine();
