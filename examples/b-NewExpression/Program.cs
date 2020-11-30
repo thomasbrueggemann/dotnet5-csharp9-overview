6998f567125cfc4d4c74a486619105d847600df9
Thomas Brüggemann <thomas.brueggemann@maersk.com>
2020-11-30 14:50:59 +0100

    Adds more examples

--- examples/b-NewExpression/Program.cs
+++ examples/b-NewExpression/Program.cs
@@ -1,12 +1,12 @@
 ﻿/*
-  _   _                 ______                              _             
- | \ | |               |  ____|                            (_)            
- |  \| | _____      __ | |__  __  ___ __  _ __ ___  ___ ___ _  ___  _ __  
- | . ` |/ _ \ \ /\ / / |  __| \ \/ / '_ \| '__/ _ \/ __/ __| |/ _ \| '_ \ 
+  _   _                 ______                              _
+ | \ | |               |  ____|                            (_)
+ |  \| | _____      __ | |__  __  ___ __  _ __ ___  ___ ___ _  ___  _ __
+ | . ` |/ _ \ \ /\ / / |  __| \ \/ / '_ \| '__/ _ \/ __/ __| |/ _ \| '_ \
  | |\  |  __/\ V  V /  | |____ >  <| |_) | | |  __/\__ \__ \ | (_) | | | |
  |_| \_|\___| \_/\_/   |______/_/\_\ .__/|_|  \___||___/___/_|\___/|_| |_|
-                                   | |                                    
-                                   |_|                                                      
+                                   | |
+                                   |_|
  */
 
 /* WHAT
@@ -14,14 +14,14 @@
 - C# 9 introduces target typed new expressions. What does that mean?
 - "Target typing" = an expression gets its type from the context of where it’s being used
 - null and lambda expressions are always target typed
-- new expressions have always required a type to be specified (except for implicitly typed array expressions). 
+- new expressions have always required a type to be specified (except for implicitly typed array expressions).
 - In C# 9.0 you can leave out the type if there’s a clear type that the expression is being assigned to.
-    
+
 */
 
 /* WHY
 
-At least in C&C we prefer to use `var` and use the actual type with the new expression, 
+At least in C&C we prefer to use `var` and use the actual type with the new expression,
 for easier readability at the beginning of the line:
 
 var container = Container();
@@ -33,7 +33,7 @@ When you have a lot of repetition, such as in an array or object initializer:
 
 using System;
 
-Container container = new(tons: 123);
+Container container = new(123);
 Vessel vessel = new(container);
 
 DateTime datetime = new(2020, 12, 4);
@@ -68,7 +68,7 @@ public class Container
 }
 
 public class Vessel
-{ 
+{
     public Vessel(Container c)
     {
     }
---
+++ examples/e-OrAndExpressions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace e_OrAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
